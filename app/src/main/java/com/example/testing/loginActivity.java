78c6521c9969aa05c66ff215d3fdfcf0060992a8
doronbb <57265470+doronbb@users.noreply.github.com>
2024-09-24 12:32:55 +0300

    initial commit

---
+++ app/src/main/java/com/example/testing/loginActivity.java
@@ -0,0 +1,53 @@
+package com.example.testing;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class loginActivity extends AppCompatActivity {
+
+    EditText etEmail;
+    EditText etPassword;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        Intent logintent = new Intent(loginActivity.this, mainActivity.class);
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //if(!etEmail.getText().toString().isEmpty() && !etPassword.getText().toString().isEmpty()){
+                    loginActivity.this.startActivity(logintent);
+                //}
+
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/testing/mainActivity.java
@@ -0,0 +1,33 @@
+package com.example.testing;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class mainActivity extends AppCompatActivity {
+
+    TextView txtvMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //Intent logintent = new Intent(mainActivity.this, loginActivity.class);
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        txtvMessage = findViewById(R.id.tvMessage);
+        txtvMessage.setText(R.string.hello);
+
+    }
+}
